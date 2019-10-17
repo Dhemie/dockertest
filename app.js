8082a735946f16cd36fe7025b82e622232169712
Demilade Oyewusi <doyewusi@terragonltd.com>
2019-10-17 11:32:06 +0100

    run with dockerfile

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get("/",(req,res)=>{
-    res.send("okay");
+    res.send("Application Deployed Successfully!");
 })
 
-
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+s
